368f14b8032cd376a37a7fec93e835dd1b5764f0
Borche <borosoposki@yahoo.com>
2021-01-26 20:10:18 +0100

    fix

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 
+cd public
+npm install
+npm run build
+cd -
+
+# START SERVICES
 node services/auth/index.js &
 node services/storage/index.js &
-node services/user/index.js &
+node services/users/index.js &
+
+# START PROXY
+node services/proxy/index.js &
